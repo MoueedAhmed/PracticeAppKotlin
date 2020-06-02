87a1a0651c9fc3af6279a9b5a8cad3551ed99277
Rao Moueed Ahmed <moueedrao@gmail.com>
2020-06-02 23:07:38 +0500

    Creating a Custom Toast View

--- app/src/main/java/com/amoueed/practiceappkotlin/MainActivity.kt
+++ app/src/main/java/com/amoueed/practiceappkotlin/MainActivity.kt
@@ -2,10 +2,25 @@ package com.amoueed.practiceappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layout = layoutInflater.inflate(R.layout.custom_toast, custom_toast_container)
+        val text: TextView = layout.findViewById(R.id.text)
+        text.text = "This is a custom toast"
+        with (Toast(applicationContext)) {
+            setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+            duration = Toast.LENGTH_LONG
+            view = layout
+            show()
+        }
     }
 }
