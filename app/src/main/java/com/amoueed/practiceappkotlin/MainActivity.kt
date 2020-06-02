7c8917fdf80e4b56d1678eaf35c782bb4599875e
Rao Moueed Ahmed <moueedrao@gmail.com>
2020-06-02 22:51:00 +0500

    reset commit

--- app/src/main/java/com/amoueed/practiceappkotlin/MainActivity.kt
+++ app/src/main/java/com/amoueed/practiceappkotlin/MainActivity.kt
@@ -2,24 +2,10 @@ package com.amoueed.practiceappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
-import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val layout = layoutInflater.inflate(R.layout.custom_toast,custom_toast_container)
-        val text: TextView = layout.findViewById(R.id.text)
-        text.text = "This is a custom toast"
-        with (Toast(applicationContext)) {
-            duration = Toast.LENGTH_LONG
-            view = layout
-            show()
-        }
     }
 }
